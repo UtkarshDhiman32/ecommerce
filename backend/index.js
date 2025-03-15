37291ad5f3384dcc8e58e38d3bab6b82666d7c7c
Utkarsh Dhiman <141824699+UtkarshDhiman32@users.noreply.github.com>
2025-03-15 22:06:00 +0530

    change my port

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const port = 4000;
+const port =  process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -11,6 +11,9 @@ const { error, log } = require("console");
 
 app.use(express.json());
 app.use(cors());
+
+
+
 
 //database connection
 mongoose.connect(
